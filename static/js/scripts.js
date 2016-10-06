bc5f8aac34296482bd9e7c1cac2ca36afc0ef8e8
Nathália Xavier <nathaliazxavier@gmail.com>
2016-10-06 14:40:58 -0300

    latex

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,10 +1,12 @@
+
 $(function () {
-    $(".latex").latex();
-  });
-  $(document).ready( function() {
-    $(".page footer").each(function(index){
-        var total = $('.page footer').length;
-        $(this).find('.this-index').text(index + 1);
-        $(this).find('.total-index').text(total);
-    });
-  });
+	$(".latex").latex();
+});
+
+$(document).ready( function() {
+	$(".page footer").each(function(index){
+	    var total = $('.page footer').length;
+	    $(this).find('.this-index').text(index + 1);
+	    $(this).find('.total-index').text(total);
+	});
+});
